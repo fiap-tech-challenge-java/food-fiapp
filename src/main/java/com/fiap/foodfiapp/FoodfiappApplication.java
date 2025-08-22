4efbeefc1fa171c300645fc8f8dec3ef7f22691f
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2025-08-21 22:30:40 -0300

    first commit

---
+++ src/main/java/com/fiap/foodfiapp/FoodfiappApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.foodfiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodfiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodfiappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/foodfiapp/FoodfiappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.foodfiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodfiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
