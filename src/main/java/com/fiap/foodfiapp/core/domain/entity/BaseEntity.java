5ee6535516bbb3b32be8bfce26cf9dd430b10a0e
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2025-09-04 12:07:11 -0300

    feat: ajustando as interfaces

--- src/main/java/com/fiap/foodfiapp/core/domain/entity/BaseEntity.java
+++ src/main/java/com/fiap/foodfiapp/core/domain/entity/BaseEntity.java
@@ -1,13 +1,19 @@
 package com.fiap.foodfiapp.core.domain.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.OffsetDateTime;
 
+@MappedSuperclass
+@Getter
+@Setter
 public abstract class BaseEntity {
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreationTimestamp
--- src/main/java/com/fiap/foodfiapp/infrastructure/persistence/springdata/UserSpringDataRepository.java
+++ src/main/java/com/fiap/foodfiapp/infrastructure/persistence/springdata/UserSpringDataRepository.java
@@ -2,7 +2,7 @@ package com.fiap.foodfiapp.infrastructure.persistence.springdata;
 
 import com.fiap.foodfiapp.infrastructure.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
+
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,6 +10,4 @@ import java.util.UUID;
 // O adapter UserRepositoryAdapter faz a ponte entre UserSpringDataRepository e UserRepository.
 public interface UserSpringDataRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmail(String email);
-    void deleteById(UUID id);
-
 }
